26c3e25689e78758635dee00580bef8dd6499d30
iamarif1 <iamarif121@gmail.com>
2021-06-15 12:13:28 +0600

    Navbar Completed

--- src/App.js
+++ src/App.js
@@ -1,12 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import "./App.css";
+import Navbar from './components/Navbar/Navbar';
 
 function App() {
-  return (
-    <div className="App">
-      
-    </div>
-  );
+   return (
+     <Router>
+       <Navbar></Navbar>
+       {/* <Switch>
+         <Route path="/incubator">
+           <Incubator></Incubator>
+         </Route>
+         <Route path="/learnPage">
+           <LearnPage></LearnPage>
+         </Route>
+         <Route path="/userDashboard/:id">
+           <UserDashboard></UserDashboard>
+         </Route>
+         <Route path="/courseItem/:_id">
+           <CourseItems></CourseItems>
+         </Route>
+         <Route path="/courseActivities/:__id">
+           <CourseActivities></CourseActivities>
+         </Route>
+         <Route exact path="/">
+           <Home></Home>
+         </Route>
+       </Switch> */}
+     </Router>
+   );
 }
 
 export default App;
