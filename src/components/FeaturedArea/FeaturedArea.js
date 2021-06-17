b45b8203f48f03c333fdb93bb6c6195af3ee6d46
iamarif1 <iamarif121@gmail.com>
2021-06-17 07:17:05 +0600

    FeaturedStartups Added

--- src/components/FeaturedArea/FeaturedArea.js
+++ src/components/FeaturedArea/FeaturedArea.js
@@ -1,16 +1,16 @@
 import React from 'react';
-import FeaturedStartups from '../FeaturedStartups/FeaturedStartups';
 import FeaturedSupport from '../FeaturedSupport/FeaturedSupport';
 import UpcomingStartups from '../UpcomingStartups/UpcomingStartups';
+import FeaturedStartups from './../FeaturedStartups/FeaturedStartups';
 
 const FeaturedArea = () => {
     return (
         <section className="features">
             <div>
                 <div className="row">
-                    <FeaturedStartups></FeaturedStartups>
+                    <FeaturedStartups/>
                     <FeaturedSupport/>
-                    <UpcomingStartups></UpcomingStartups>
+                    <UpcomingStartup/>
                 </div>
             </div>
         </section>
